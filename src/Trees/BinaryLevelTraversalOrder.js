8ecc1f47337f455632a22b4506903af62282f602
davishooly <hoolykim@gmail.com>
2021-01-17 13:06:52 +0300

    chore(trees): binary traversals

---
+++ src/Trees/BinaryLevelTraversalOrder.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+
+
+//  stack or queue
+//  stack -> DFS(depth first search) -> for every node you traverse deeply to a leafNode(no child nodes)
+//  queue -> BFS(breath first search) -> visit a node closest to the root that you havent visited.
+
+//  traverse top level nodes
+    const queue = [root];
+    const output = [];
+
+    if( !root){
+        return [];
+    }
+
+//  adding level nodes to queue
+
+    while(queue.length) {
+        const temp = [];
+        const size = queue.length;
+
+        for(i= 0; i < size; i++ ){
+            const node = queue.shift();
+            if(node.left){queue.push(node.left)}
+            if(node.right){ queue.push(node.right)}
+            temp.push(node.val);
+        }
+
+        // update node values
+        if(temp.length ) {
+            output.push(temp)
+        }
+
+    }
+
+    return output;
+};
---
+++ src/hammingNumbers/index.js
@@ -0,0 +1,8 @@
+// write a function that computes  the nth smallest Hamming number
+
+export const hamming = () => {
+   //  TODO program here
+    const smooth =  [2,3,5]
+};
+
+hamming(4);
