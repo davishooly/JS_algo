bd895718beb8fba1e219c0035f0355775b79da16
davishooly <hoolykim@gmail.com>
2021-02-10 19:22:30 +0300

    chore(twoSum): refactor two sum

--- src/twoSum/index.js
+++ src/twoSum/index.js
@@ -3,24 +3,20 @@
  * @param {number} target
  * @return {number[]}
  */
-export const twoSum = function(nums, target) {
+var twoSum = function(nums, target) {
 
-  for( let i = 0 ; i < nums.length ; i++){
-    for(let j = 1 ; j < nums.length; j++) {
-      if ( nums[i] + nums[j] === target) {
-        if( nums[i]  === nums[j]){
-          const first = nums.indexOf(nums[i]);
-          const second = nums.indexOf(nums[i], first + 1);
-          if(second === -1) {
-            continue
-          } else {
-            return [i , second]
-          }
-        }
-        return [i , j]
+  if(!nums.length) return  [];
+
+  for(let i = 0; i < nums.length; i++){
+    for(let j = i + 1 ; j < nums.length; j++){
+      const twoSum = nums[i] + nums[j];
+      if (twoSum === target){
+        return [ i , j]
       }
     }
+
   }
 
 };
+
 
