74640ee1d56d7758bfa904b5d4a8a07f6a1967e3
davishooly <hoolykim@gmail.com>
2021-03-02 06:32:16 +0300

    append linked list iteration approach

--- src/LinkedList/linkedListAppend.js
+++ src/LinkedList/linkedListAppend.js
@@ -3,6 +3,7 @@ function Node(data) {
     this.next = null;
 }
 
+// recursion approach
 function append(listA, listB) {
     // Your code goes here.
     // Remember to return the head of the list.
@@ -14,3 +15,24 @@ function append(listA, listB) {
     head.next= append(head.next,listB);
     return head;
 }
+
+// iteration approach
+
+function appendIteration(listA, listB) {
+    // Your code goes here.
+    // Remember to return the head of the list.
+
+    if(!listA && !listB) return null;
+    if(!listA || !listB) return  listA || listB;
+
+    let head = listA;
+     while(head.next) {
+         head = head.next
+     }
+
+     head.next = listB;
+
+    return listA;
+}
+
+
